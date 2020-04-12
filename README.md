fff6a92480a3eecef36a85026772de753d4612d1
Admin <1318399699@qq.com>
2020-04-12 22:43:19 +0800

    上传api服务项目基础代码

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# base-api
-一个基础的api服务项目
+# base-api 一个基础的api服务项目
+### base-dependencies:统一Maven依赖版本管理
+### base-common:公共工具类/实体类
+### base-project:api服务项目
+* 添加代码生成插件
+* logback配置
---
+++ base-project/src/main/java/com/lwl/base/project/BaseProjectApplication.java
@@ -0,0 +1,15 @@
+package com.lwl.base.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LinWenLi
+ */
+@SpringBootApplication
+public class BaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProjectApplication.class, args);
+    }
+}

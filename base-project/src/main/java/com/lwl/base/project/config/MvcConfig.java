25af1610e3f664cee313682a5b6808d46cc50030
Admin <1318399699@qq.com>
2020-04-26 00:24:52 +0800

    添加跨域配置

---
+++ base-project/src/main/java/com/lwl/base/project/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.lwl.base.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
